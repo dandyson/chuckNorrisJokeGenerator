99a39fe3761261566108a15aa42bd8ed9f21b83c
terraformmedia <31185372+terraformmedia@users.noreply.github.com>
2020-04-22 17:12:02 +0100

    Added files to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
+import oval from './images/oval.svg';
 
-function App() {
+const App = () => {
+
+  const [joke, setJoke] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [fetching, setFetching] = useState(false);
+  const [inProp, setInProp] = useState(false);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true)
+      const result = await axios("https://api.chucknorris.io/jokes/random");
+      //console.log(result.data);
+      setJoke(`${result.data.value}`)
+      setLoading(false)
+    }
+    fetchData();
+  }, [fetching])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className="jokeDiv">
+      <img src="https://images02.military.com/sites/default/files/styles/full/public/media/veteran-jobs/content-images/2016/03/chucknorris.jpg?itok=_J3M4O-S" id="chuckImg"></img>
+      <h1>Chuck Norris <br />Joke Generator</h1>
+      <button onClick={() => setFetching(!fetching)}> In Soviet Russia, button clicks YOU</button><br />
+      {loading ? (
+        <img
+          className="loadingImg"
+          src={oval}
+          alt="loader"
+          height="20px"
+        />
+      ) : (
+
+          <h3 className="theJoke bounceIn">{joke}</h3>
+
+        )}
+    </div >
+  )
 }
 
 export default App;
